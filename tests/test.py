427b123f5f1192fbc0d121b10560987f35268c27
an-maksimov <an.maksimov@gmail.com>
2016-09-08 21:42:51 +0400

    test

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,8 @@
 import requests
 import json
+import simplejson
 
 url = "http://192.168.0.249/api/status"
 method = "GET"
 response = requests.request(method, url)
 payload = json.loads(response.content)
-print payload['code']
